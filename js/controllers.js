9e53b58b84d48d502386731df6d4715606a0736e
Faisal AlQasimi <falqas@gmail.com>
2016-06-06 19:01:13 -0700

    feat: add video modals

--- js/controllers.js
+++ js/controllers.js
@@ -1,10 +1,62 @@
-(function () {
-'use strict';
+(function() {
+  'use strict';
 
-angular.module('myApp', [])
-  .controller('exhibitsCtrl', ['$scope', function ($scope) {
-    $scope.exhibits = ["Exhibit 1.png", "Exhibit 2.png", "Exhibit 3.png"]
+  angular.module('myApp', [])
+      .controller('exhibitsCtrl', ['$scope', function($scope) {
 
-}]);
+
+          $scope.currentVideo = "";
+          $scope.showModal = !1;
+          $scope.exhibits = [
+
+              {
+                  id: 1,
+                  name: "Season 0: KTMA",
+                  image: "Exhibit 1.png",
+                  youtubeId: "wd5nFd3utLg"
+              }, {
+                  id: 2,
+                  name: "Comedy Central - The Golden Years",
+                  image: "Exhibit 2.png",
+                  youtubeId: "wZn4Y0xPSPY"
+              }, {
+                  id: 3,
+                  name: "Sci-Fi, Crow's Voice, & Ram Chips",
+                  image: "Exhibit 3.png",
+                  youtubeId: "DLzxrzFCyOs"
+              }
+          ];
+
+
+
+        $scope.updateVideo = function(videoId){
+
+          $scope.currentVideo = "https://www.youtube.com/embed/" + videoId;
+          console.log('videoId:', videoId);
+          var c = document.querySelectorAll(".video-container");
+          angular.element(c).html("<iframe src='https://www.youtube.com/embed/" + videoId + "?autoplay=1' width='640' height='480' frameborder='0' ></iframe>")
+
+        }
+
+        $scope.removeVideo = function(videoId){
+          $scope.currentVideo = "";
+          console.log('removeVideo')
+          var c = document.querySelectorAll(".video-container");
+
+          angular.element(c).html("<iframe src='https://www.youtube.com/embed/" + videoId + "?autoplay=1' width='640' height='480' frameborder='0' ></iframe>")
+
+
+          // function pauseVideo() {
+          //   if (ytplayer) {
+          //    ytplayer.pauseVideo();
+          //  }
+          // }
+
+          // pauseVideo();
+
+        }
+
+
+      }]);
 
 })();
